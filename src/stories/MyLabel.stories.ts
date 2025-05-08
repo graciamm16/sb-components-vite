8bea7a14832674cb1a49910bf17537fdd7e46031
Gracia <graciamartin007@gmail.com>
2025-05-08 11:58:25 +0200

    storybook listo

---
+++ src/stories/MyLabel.stories.ts
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { MyLabel } from "../components/MyLabel";
+
+
+// Información que vamos a tener sobre la página
+const meta = {
+    title: 'UI/labels/MyLabel',
+    component: MyLabel,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'centered'
+    },
+    argTypes: {
+        size: {control: 'inline-radio'},
+        fontColor: {control: 'color'}
+    }
+} satisfies Meta<typeof MyLabel>;
+
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Basic: Story = {
+    args:{
+        label: 'Basic Label'
+    }
+};
+
+export const AllCaps: Story = {
+    args:{
+        label: 'All Caps Label',
+        allCaps: true
+    }
+};
+
+export const Secondary: Story = {
+    args:{
+        label: 'Secondary Label',
+        color: 'text-secondary'
+    }
+};
+
+export const CustomColor: Story = {
+    args:{
+        label: 'Custom Color Label',
+        fontColor: '#5517ac'
+    }
+};
